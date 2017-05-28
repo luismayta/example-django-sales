b506704900db13834e1a908b1da35bc1266a16bb
@slovacus <slovacus@gmail.com>
2017-05-28 00:15:05 -0500

    chore: implement app common custom user (#2)

---
+++ src/customuser/admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import MyUser
+
+
+@admin.register(MyUser)
+class MyUserAdmin(UserAdmin):
+
+    list_display = (
+        'email',
+        'id',
+        'first_name',
+        'last_name',
+        'is_active',
+        'date_joined',
+        'created',
+        'modified',
+    )
+    list_filter = (
+        'is_active',
+    )
+    fieldsets = (
+        (None, {'fields': (
+            'email',
+            'password'
+        )}),
+        ('Personal info', {'fields': (
+            'first_name',
+            'last_name',
+            'date_joined',
+        )}),
+        ('Permissions', {'fields': (
+            'is_active',
+        )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'first_name',
+                'last_name',
+                'password1',
+                'password2',
+            )
+        }),
+    )
+    search_fields = (
+        'email',
+        'first_name',
+        'last_name',
+    )
+    filter_horizontal = ()
+    ordering = ('-created',)
---
+++ src/customuser/apps.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class CustomuserConfig(AppConfig):
+    name = 'customuser'
