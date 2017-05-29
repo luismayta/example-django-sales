4621e7fd7e967e482697bf70ef68981b620f0f8d
@slovacus <slovacus@gmail.com>
2017-05-29 13:25:16 -0500

    !#3 Fix: forms of customuser

--- src/customuser/forms.py
+++ src/customuser/forms.py
@@ -26,29 +26,29 @@ class UserSignupForm(CleanNamesForm):
     fields, plus a repeated password.
     """
 
-    password1 = forms.CharField(
+    password = forms.CharField(
         label='Password',
         widget=forms.PasswordInput,
         max_length=255,
         min_length=6,
     )
-    password2 = forms.CharField(
+    password1 = forms.CharField(
         label='Password confirmation',
         widget=forms.PasswordInput,
     )
 
-    def clean_password2(self):
+    def clean_password1(self):
         # Checks that the two password entries match.
+        password = self.cleaned_data.get('password')
         password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
+        if password and password1 and password != password1:
             raise forms.ValidationError('Passwords don\'t match')
-        return password2
+        return password1
 
     def save(self, commit=True):
         # Save the provided password in hashed format.
         user = super(UserSignupForm, self).save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
+        user.set_password(self.cleaned_data['password'])
         if commit:
             user.save()
         return user
