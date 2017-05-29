cb09fd75eacc698665492cbbd318c9fec2ccd1e9
@slovacus <slovacus@gmail.com>
2017-05-28 23:48:20 -0500

    chore: implement serializer custom user (#4)

--- src/customuser/views.py
+++ src/customuser/views.py
@@ -3,6 +3,10 @@
 
 from customuser.models import MyUser
 from rest_framework import viewsets
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken as ObtainToken
+from rest_framework.response import Response
 
 from .serializers import MyUserSerializer
 
@@ -37,3 +41,23 @@ class MyUserViewSet(viewsets.ModelViewSet):
         if password:
             user.set_password(password)
             user.save()
+
+
+class ObtainAuthToken(ObtainToken):
+    """Overrides the default ObtainAuthToken for token auth."""
+
+    def post(self, request):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user': {
+                'id': user.id,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'is_active': user.is_active,
+            },
+        })
