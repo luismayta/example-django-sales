0e6b346b74f01c2b0c2b99113186f8530045193e
@slovacus <slovacus@gmail.com>
2017-05-29 08:37:01 -0500

    !#4 Add: import class test drf

--- src/features/steps/login.py
+++ src/features/steps/login.py
@@ -1,8 +1,8 @@
+from test.drf import APIClient
 from test.factories.user import UserFactory
 
 from behave import given
 from hamcrest import assert_that, not_none
-from rest_framework.test import APIClient
 
 
 @given(u'The user with "{email}" and password "{password}"')
@@ -13,8 +13,8 @@ def the_user_with_and_password(context, email, password):
     assert_that(user, not_none)
     context.user = user
     response = APIClient.login(
-        username=user.email,
-        password='P@ssw0rd'
+        username=email,
+        password=password
     )
 
     assert_that(response, not_none)
