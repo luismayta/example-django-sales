1d87c75783f9535432729f94008c44fe114f96af
@slovacus <slovacus@gmail.com>
2017-05-29 12:13:38 -0500

    !#3 Fix: clean unnecesary urls

--- src/sales/urls.py
+++ src/sales/urls.py
@@ -1,7 +1,6 @@
 from customuser.views import MyUserViewSet, ObtainAuthToken
 from django.conf.urls import include, url
 from django.contrib import admin
-from home.views import HomeView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -22,9 +21,6 @@ urlpatterns = [
     ),
     # Token authentication.
     url(r'^api-token-auth/', ObtainAuthToken.as_view()),
-    # Home page.
-    url(r'^$', HomeView.as_view(), name='home_page'),
-    ##################################################
-    # /home/*
-    url(r'^home/', include('home.urls')),
+    # Core app.
+    url(r'^', include('home.urls', namespace='home_app')),
 ]
